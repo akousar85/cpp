f33068d8a67e34ceb4823862bc0c8dbbe56979a4
adeelakousar <adeela.kousar@jjay.cuny.edu>
2018-02-15 22:42:17 -0500

    finished helloworld prgoram

--- foo.cpp
+++ foo.cpp
@@ -1,9 +1,50 @@
-#include <iostream>
 
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-int main () {
-    cout << "hello world 111" << endl;
+int main()
+
+{
+
+int num;
+char  deposit;
+char type;
+float amount;
+
+public:
+
+    void create_account();
+    void show_account)() const;
+    void deposit (int);
+    {
+    void account: :create_account()
 }
 
+cout << "Enter account no:\n";
+    cin >> num;
+
+cout << "Enter deposit ;\n";
+    cin >> deposit;
+
+cout << "Enter account type:\n";
+    cin >> type;
+
+cout << "Enter balance amount:\n";
+    cin >> amount;
+
+    {
+        void account::show_account() const
+
+    }
+    cout << "account no: " << num;
+
+    cout << "enter depoisit: "<< deposit;
+
+    cout << "Enter account type:"<< type;
+
+    cout << "Enter balance amount:"<< amount;
+
+return 0;
+}
